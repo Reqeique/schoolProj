aeae0b1561a1cf3e9805884fba0201bea5c4d5dd
Matias <108193723+matias-321@users.noreply.github.com>
2022-12-18 17:49:14 +0300

    Add files via upload

--- client/Betetseb Academy Website/tailwind.config.cjs
+++ client/Betetseb Academy Website/tailwind.config.cjs
@@ -3,19 +3,21 @@ module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     fontFamily: {
-      crimson: "'Crimson Pro', serif;"
+      crimson: "'Crimson Pro', serif;",
     },
     colors: {
-      'colorPrimary': '#343B5E',
-      'colorPrimarySurface': "#FFFFFF",
-      'colorPrimarySurfaceDark': "#131313",
-      'colorPrimaryDark': '#9CA9F1'
+      colorPrimary: "#343B5E",
+      colorPrimarySurface: "#FFFFFF",
+      colorPrimarySurfaceDark: "#131313",
+      colorPrimaryDark: "#9CA9F1",
+      colorOverlay: "rgba(0, 0, 0, 0.6)",
+      colorOverlay2: "rgba(0, 0, 0, 0.6)",
     },
     extend: {
       backgroundImage: {
-        'image_home': "url('./src/assets/image_home.jpg')"
-      }
+        image_home: "url('./src/assets/image-home.jpg')",
+      },
     },
   },
   plugins: [],
-}
+};
